2f3dc9d3e8a52510707e2d048a95b3e839b0781e
Miguel Riem de Oliveira <m.riem.oliveira@gmail.com>
2016-12-03 12:37:39 +0000

    adding a . to rosrc

--- scripts/install_scripts.bash
+++ scripts/install_scripts.bash
@@ -3,7 +3,7 @@ DIR=`pwd`
 echo $DIR
 
 #To be called by windows
-ln -si $DIR/rosrc $HOME/rosrc
+ln -si $DIR/.rosrc $HOME/.rosrc
 ln -si $DIR/start_recording.bash $HOME/start_recording.bash
 ln -si $DIR/stop_recording.bash $HOME/stop_recording.bash
 
