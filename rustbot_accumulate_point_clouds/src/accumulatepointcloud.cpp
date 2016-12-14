eb19c9798c6cd56e73cdade711f36cf699d98bbf
Miguel Riem de Oliveira <m.riem.oliveira@gmail.com>
2016-12-14 19:49:14 +0000

    trying to find bug in acc cloud

--- rustbot_accumulate_point_clouds/src/accumulatepointcloud.cpp
+++ rustbot_accumulate_point_clouds/src/accumulatepointcloud.cpp
@@ -17,27 +17,58 @@ std::string filename = "output.pcd";
 
 pcl::PointCloud<PointT>::Ptr accumulated_cloud;
 pcl::PointCloud<PointT>::Ptr accumulated_cloud_filtered;
+
+pcl::PointCloud<PointT>::Ptr cloud;
+pcl::PointCloud<PointT>::Ptr tmp_cloud;
+pcl::PointCloud<PointT>::Ptr cloud_transformed;
+
 pcl::VoxelGrid<PointT> grid;
 
 tf::TransformListener *p_listener;
+boost::shared_ptr<ros::Publisher> pub;
 
 void cloud_open_target (const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-    pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
-    pcl::PointCloud<PointT>::Ptr cloud_filtered(new pcl::PointCloud<PointT>);
-    pcl::PointCloud<PointT>::Ptr cloud_transformed(new pcl::PointCloud<PointT>);
+
     pcl::fromROSMsg (*msg, *cloud);
 
-    grid.setInputCloud (cloud);
-    grid.filter (*cloud_filtered);
+    ROS_INFO("Size of cloud = %ld", cloud->points.size());
 
-    pcl_ros::transformPointCloud(ros::names::remap("/map"), *cloud_filtered, *cloud_transformed, *p_listener);
-    //ROS_INFO("cloud_transformed->points.size() = %ld", cloud_transformed->points.size());
-    //cloud_transformed->header.frame_id = "/map";
+    printf("LINE=%d\n",__LINE__);
+    pcl_ros::transformPointCloud(ros::names::remap("/map"), *cloud, *cloud_transformed, *p_listener);
+    printf("LINE=%d\n",__LINE__);
 
+
+    ROS_INFO("Size of input cloud = %ld", cloud_transformed->points.size());
+    printf("LINE=%d\n",__LINE__);
     (*accumulated_cloud) += (*cloud_transformed);
-    accumulated_cloud->header.frame_id;
+    printf("LINE=%d\n",__LINE__);
+
+
     ROS_INFO("Size of accumulated_cloud = %ld", accumulated_cloud->points.size());
+    (*tmp_cloud) = *accumulated_cloud;
+    printf("LINE=%d\n",__LINE__);
+    grid.setInputCloud (tmp_cloud);
+    printf("LINE=%d\n",__LINE__);
+    grid.filter (*accumulated_cloud);
+    printf("LINE=%d\n",__LINE__);
+
+    ROS_INFO("Size of accumulated_cloud = %ld", accumulated_cloud->points.size());
+
+    
+    sensor_msgs::PointCloud2 msg_out;
+    printf("LINE=%d\n",__LINE__);
+    pcl::toROSMsg (*cloud, msg_out);
+    //pcl::toROSMsg (*accumulated_cloud, msg_out);
+    printf("LINE=%d\n",__LINE__);
+    //msg_out.header.stamp = ros::Time::now();
+    //printf("LINE=%d\n",__LINE__);
+    //msg_out.is_dense = 0;
+    printf("LINE=%d\n",__LINE__);
+    pub->publish(msg_out);
+    printf("LINE=%d\n",__LINE__);
+
+    ROS_INFO("Size of msg_out = %d", msg_out.width);
 }
 
 
@@ -45,21 +76,32 @@ int main (int argc, char** argv)
 {
 
     // Initialize ROS
-    ros::init (argc, argv, "accumulatepointcloud2file");
+    ros::init (argc, argv, "accumulatepointcloud");
 
     // Node Handle
     ros::NodeHandle nh;
 
-    ros::param::get("~filename", filename);
-
-
     accumulated_cloud = (pcl::PointCloud<PointT>::Ptr) new pcl::PointCloud<PointT>;
+    cloud = (pcl::PointCloud<PointT>::Ptr) (new pcl::PointCloud<PointT>);
+    cloud_transformed = (pcl::PointCloud<PointT>::Ptr) (new pcl::PointCloud<PointT>);
+    tmp_cloud = (pcl::PointCloud<PointT>::Ptr) (new pcl::PointCloud<PointT>);
+
+
+
+    accumulated_cloud->header.frame_id = ros::names::remap("/map");
     p_listener = (tf::TransformListener*) new tf::TransformListener;
     
-    grid.setLeafSize (0.03f, 0.03f, 0.03f);
+    grid.setLeafSize (0.1f, 0.1f, 0.1f);
+    //grid.setInputCloud (accumulated_cloud);
+    //grid.filter (*accumulated_cloud_filtered);
+
+    ros::Duration(2).sleep();
 
     // Create a ROS subscriber for the input point cloud
     ros::Subscriber sub_target = nh.subscribe ("input", 1, cloud_open_target);
+
+    pub = (boost::shared_ptr<ros::Publisher>) new ros::Publisher;
+    *pub = nh.advertise<sensor_msgs::PointCloud2>("accumulated_point_cloud", 1);
 
 
     while (ros::ok())
@@ -67,11 +109,5 @@ int main (int argc, char** argv)
         // Spin
         ros::spinOnce();
     }
-
-    grid.setInputCloud (accumulated_cloud);
-    grid.filter (*accumulated_cloud_filtered);
-
-    printf("Saving to file %s\n", filename.c_str());;
-    pcl::io::savePCDFileASCII (filename, *accumulated_cloud_filtered);
 
 }
