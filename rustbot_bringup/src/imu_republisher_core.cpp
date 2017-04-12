f1089bf14a6420f7848c36d9afd52a2407de5728
Filipe Neves dos Santos <filipe.n.santos@inesctec.pt>
2017-04-12 16:49:36 +0100

    Last update

--- rustbot_bringup/src/imu_republisher_core.cpp
+++ rustbot_bringup/src/imu_republisher_core.cpp
@@ -53,14 +53,14 @@ void IMU_replublish::IMU_MAG_data_messageCallback(const sensor_msgs::MagneticFie
     tf::Quaternion qt_tf;
     obs_mat.getRotation(qt_tf);
 
-    std::cout << "IMU" << atan2(msg->magnetic_field.y, msg->magnetic_field.x) * 180.0/ 3.14 << std::endl;
+    //std::cout << "IMU" << (-atan2(msg->magnetic_field.y, msg->magnetic_field.x)+1.57)* 180.0/ 3.14 << std::endl;
     IMU_data.orientation.x=qt_tf.getX();
     IMU_data.orientation.y=qt_tf.getY();
     IMU_data.orientation.z=qt_tf.getZ();
     IMU_data.orientation.w=qt_tf.getW();
-    IMU_data.orientation_covariance.at(0)=1.0;
-    IMU_data.orientation_covariance.at(4)=1.0;
-    IMU_data.orientation_covariance.at(8)=1.0;
+    IMU_data.orientation_covariance.at(0)=0.01;
+    IMU_data.orientation_covariance.at(4)=0.01;
+    IMU_data.orientation_covariance.at(8)=0.1;
     IMU_data.header.frame_id.assign("imu_frame");
     publishMessage(&pub_message_IMU);
     imu_deg=1;
@@ -72,9 +72,15 @@ void IMU_replublish::IMU_MAG_data_messageCallback(const sensor_msgs::MagneticFie
  *------------------------------------------------------------------*/
 void IMU_replublish::IMU_yawfromGPSVelocity_messageCallback(const geometry_msgs::TwistStampedConstPtr &msg){
 
-     std::cout << "GPS "<< msg->twist.linear.x << " | "<< msg->twist.linear.y << " deu=" << atan2(msg->twist.linear.x,msg->twist.linear.y) * 180.0/3.14  << std::endl;
+    // std::cout << "GPS "<< msg->twist.linear.x << " | "<< msg->twist.linear.y << " deu=" << atan2(msg->twist.linear.x,msg->twist.linear.y) * 180.0/3.14  << std::endl;
 
-     yaw_gps=atan2(msg->twist.linear.y,msg->twist.linear.x);
+     //
+    // ENU ---> NED
+    // roll ---->roll
+    // pitch ----->-pitch
+    // yaw ----->-yaw+1.57.
+     yaw_gps=-atan2(msg->twist.linear.x,msg->twist.linear.y)+1.57;
+     // std::cout << "GPS corrigido "<< yaw_gps * 180.0/3.14  << std::endl;
 }
 
 /*--------------------------------------------------------------------
@@ -82,8 +88,10 @@ void IMU_replublish::IMU_yawfromGPSVelocity_messageCallback(const geometry_msgs:
  * Callback function for subscriber.
  *------------------------------------------------------------------*/
 void IMU_replublish::IMU_data_yawmag_messageCallback(const std_msgs::Float64ConstPtr &msg){
-  if( imu_deg==1 ) return;
-   std::cout << " deu=" << msg->data  << std::endl;
+  // std::cout << " deu=" << -msg->data+1.57*180/3.14  << std::endl;
+
+    if( imu_deg==1 ) return;
+
   tf::Matrix3x3 obs_mat;
   obs_mat.setRPY(0,0,msg->data * 3.14/180.0);
   tf::Quaternion qt_tf;
@@ -98,6 +106,7 @@ void IMU_replublish::IMU_data_yawmag_messageCallback(const std_msgs::Float64Cons
   IMU_data.orientation_covariance.at(8)=1.0;
   IMU_data.header.frame_id.assign("imu_frame");
   publishMessage(&pub_message_IMU);
+   //std::cout << " ============" <<std::endl;
 
 } // end publishCallback()
 
