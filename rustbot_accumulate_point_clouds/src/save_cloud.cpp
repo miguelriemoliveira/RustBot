fa0bca5c96ab5539b4a43621c6adc7c41feb51fe
vinicius <vinicius.vidal@engenharia.ufjf.br>
2019-03-21 22:36:49 -0300

    porra deu

--- rustbot_accumulate_point_clouds/src/save_cloud.cpp
+++ rustbot_accumulate_point_clouds/src/save_cloud.cpp
@@ -32,105 +32,58 @@ bool visual_gravada = false, termica_gravada = false;
 pcl::PointCloud<PointT>::Ptr termica_acumulada;
 pcl::PointCloud<PointT>::Ptr visual_acumulada;
 
-void save_acumulada_plus_normal_ply(const sensor_msgs::PointCloud2ConstPtr& msg)
+int mutexv, mutext; // Em 0
+
+void savevisual_ply(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-  if(!visual_gravada){
+  ros::Rate r(4);
+  if(!visual_gravada && mutexv == 0){
+    mutexv = 1; // Por hora, ninguem mais acessa a memoria da nuvem
 
     pcl::PointCloud<PointT>::Ptr recv_cloud_ptr(new pcl::PointCloud<PointT>); // Allocate memory
     pcl::fromROSMsg(*msg, *recv_cloud_ptr);
+
     if(recv_cloud_ptr->size() > 10)
       *visual_acumulada = *recv_cloud_ptr;
+
     ROS_INFO("Nuvem visual atualizada e apta para ser salva.");
-//    ROS_INFO("Recebendo dados visuais para salvar......");
-//    // Declare the pointer to the received cloud
-//    pcl::PointCloud<PointT>::Ptr recv_cloud_ptr(new pcl::PointCloud<PointT>); // Allocate memory
+    recv_cloud_ptr->clear();
+    r.sleep();
 
-//    // Pass the message to a pcl entity
-//    pcl::fromROSMsg(*msg, *recv_cloud_ptr);
-
-//    std::vector<int> indicesNAN;
-//    pcl::removeNaNFromPointCloud(*recv_cloud_ptr, *recv_cloud_ptr, indicesNAN);
-
-//    //////////////////////////// SALVAR
-//    // Hora atual
-//    ROS_INFO("Salvando o arquivo .ply na area de trabalho......");
-//    // Ver o tempo para diferenciar bags gravadas automaticamente
-//    time_t t = time(0);
-//    struct tm * now = localtime( & t );
-//    std::string year, month, day, hour, minutes, home;
-//    char const* tmp = getenv("HOME");
-//    if(tmp)
-//      home = std::string(tmp);;
-//    year    = boost::lexical_cast<std::string>(now->tm_year + 1900);
-//    month   = boost::lexical_cast<std::string>(now->tm_mon );
-//    day     = boost::lexical_cast<std::string>(now->tm_mday);
-//    hour    = boost::lexical_cast<std::string>(now->tm_hour);
-//    minutes = boost::lexical_cast<std::string>(now->tm_min );
-//    std::string date = "_" + year + "_" + month + "_" + day + "_" + hour + "h_" + minutes + "m.ply";
-
-//    // Salvando
-//    filename = home+"/Desktop/pos_processo_visual_em"+date;
-//    pcl::io::savePLYFileASCII(filename, *recv_cloud_ptr);
-
-//    ROS_INFO("Tudo correto, conferir pelos arquivos na area de trabalho !!");
-
-//    // Kill the node after saving the ptcloud
-//    visual_gravada = true;
-//  }
-//  if(visual_gravada && termica_gravada){
-//    ros::shutdown();
+    mutexv = 0;
   }
 }
 
 void savetermica_ply(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-  if(!termica_gravada){
+  ros::Rate r(4);
+  if(!termica_gravada && mutext == 0){
+    mutext = 1; // Por hora, ninguem mais acessa a memoria da nuvem
 
     pcl::PointCloud<PointT>::Ptr recv_cloud_ptr(new pcl::PointCloud<PointT>); // Allocate memory
     pcl::fromROSMsg(*msg, *recv_cloud_ptr);
+
     if(recv_cloud_ptr->size() > 10)
       *termica_acumulada = *recv_cloud_ptr;
+
     ROS_INFO("Nuvem termica atualizada e apta para ser salva.");
-//    ROS_INFO("Recebendo dados termicos para salvar......");
-//    // Declare the pointer to the received cloud
-//    pcl::PointCloud<PointT>::Ptr recv_cloud_ptr(new pcl::PointCloud<PointT>); // Allocate memory
+    recv_cloud_ptr->clear();
+    r.sleep();
 
-//    // Pass the message to a pcl entity
-//    pcl::fromROSMsg(*msg, *recv_cloud_ptr);
-
-
-//    ROS_INFO("Salvando o arquivo .ply na area de trabalho......");
-//    // Ver o tempo para diferenciar bags gravadas automaticamente
-//    time_t t = time(0);
-//    struct tm * now = localtime( & t );
-//    std::string year, month, day, hour, minutes, home;
-//    char const* tmp = getenv("HOME");
-//    if(tmp)
-//      home = std::string(tmp);
-//    year    = boost::lexical_cast<std::string>(now->tm_year + 1900);
-//    month   = boost::lexical_cast<std::string>(now->tm_mon );
-//    day     = boost::lexical_cast<std::string>(now->tm_mday);
-//    hour    = boost::lexical_cast<std::string>(now->tm_hour);
-//    minutes = boost::lexical_cast<std::string>(now->tm_min );
-//    std::string date = "_" + year + "_" + month + "_" + day + "_" + hour + "h_" + minutes + "m.ply";
-//    filename = home+"/Desktop/pos_processo_termico_em"+date;
-//    pcl::io::savePLYFileASCII(filename, *recv_cloud_ptr);
-//    ROS_INFO("Tudo correto, conferir pelo arquivo na area de trabalho !!");
-
-//    termica_gravada = true;
-//  }
-//  if(visual_gravada && termica_gravada){
-//    ros::shutdown();
+    mutext = 0;
   }
 }
 
 void salvando_nuvens(const std_msgs::BoolConstPtr& salvar){
-  if(salvar->data == true){
+  if(salvar->data == true && mutexv == 0 && mutext == 0){
+    mutexv = 1; // Por hora, ninguem mais acessa a memoria da nuvem
+    mutext = 1;
+
     // Filtrando nuvens
-//    std::vector<int> indicesNAN;
-//    pcl::removeNaNFromPointCloud(*visual_acumulada, *visual_acumulada, indicesNAN);
-//    std::vector<int> indicesNAN2;
-//    pcl::removeNaNFromPointCloud(*termica_acumulada, *termica_acumulada, indicesNAN2);
+    std::vector<int> indicesNAN;
+    pcl::removeNaNFromPointCloud(*visual_acumulada , *visual_acumulada , indicesNAN );
+    std::vector<int> indicesNAN2;
+    pcl::removeNaNFromPointCloud(*termica_acumulada, *termica_acumulada, indicesNAN2);
 
     // Ver o tempo para diferenciar bags gravadas automaticamente
     time_t t = time(0);
@@ -145,16 +98,17 @@ void salvando_nuvens(const std_msgs::BoolConstPtr& salvar){
     hour    = boost::lexical_cast<std::string>(now->tm_hour);
     minutes = boost::lexical_cast<std::string>(now->tm_min );
     std::string date = "_" + year + "_" + month + "_" + day + "_" + hour + "h_" + minutes + "m.ply";
-    ROS_INFO("Recebendo dados termicos para salvar......");
+    ROS_INFO("Recebendo dados termicos para salvar......");    
 
     if(termica_acumulada->size() > 0){
-    ROS_INFO("Salvando o arquivo .ply na area de trabalho......");
+      ROS_INFO("Salvando o arquivo .ply na area de trabalho......");
 
-    filename = home+"/Desktop/pos_processo_termico_em"+date;
-    pcl::io::savePLYFileASCII(filename, *termica_acumulada);
-    ROS_INFO("Tudo correto, conferir pelo arquivo na area de trabalho !!");
+      filename = home+"/Desktop/pos_processo_termico_em"+date;
+      pcl::io::savePLYFileASCII(filename, *termica_acumulada);
 
-    termica_gravada = true;
+      ROS_INFO("Tudo correto, conferir pelo arquivo na area de trabalho !!");
+
+      termica_gravada = true;
     } else {
       ROS_INFO("Nao ha dados termicos, nada foi salvo...");
     }
@@ -162,21 +116,32 @@ void salvando_nuvens(const std_msgs::BoolConstPtr& salvar){
     ROS_INFO("Recebendo dados visuais para salvar......");
 
     if(visual_acumulada->size() > 0){
-    ROS_INFO("Salvando o arquivo .ply na area de trabalho......");
-    // Salvando
-    filename = home+"/Desktop/pos_processo_visual_em"+date;
-    pcl::io::savePLYFileASCII(filename, *visual_acumulada);
+      ROS_INFO("Salvando o arquivo .ply na area de trabalho......");
+      // Salvando
+      filename = home+"/Desktop/pos_processo_visual_em"+date;
+      pcl::io::savePLYFileASCII(filename, *visual_acumulada);
 
-    ROS_INFO("Tudo correto, conferir pelos arquivos na area de trabalho !!");
+      ROS_INFO("Tudo correto, conferir pelos arquivos na area de trabalho !!");
 
-    // Kill the node after saving the ptcloud
-    visual_gravada = true;
+      // Kill the node after saving the ptcloud
+      visual_gravada = true;
     } else {
       ROS_INFO("Nao ha dados visuais, nada foi salvo...");
     }
+
   }
-  if(visual_gravada && termica_gravada)
+
+  mutexv = 0;
+  mutext = 0;
+
+  ros::Rate aguardar(3);
+  if(visual_gravada && termica_gravada){
+    ROS_INFO("AGUARDE PARA GRAVAR OS DADOS EM SEGURANCA...");
+    ROS_INFO("ESSA JANELA SE FECHARA EM 10 SEGUNDOS...");
+    aguardar.sleep();
     ros::shutdown();
+  }
+
 }
 
 int main(int argc, char **argv)
@@ -189,9 +154,11 @@ int main(int argc, char **argv)
   visual_acumulada  = (pcl::PointCloud<PointT>::Ptr) new pcl::PointCloud<PointT>;
   termica_acumulada = (pcl::PointCloud<PointT>::Ptr) new pcl::PointCloud<PointT>;
 
-  ros::Subscriber sub     = nh.subscribe("/accumulated_point_cloud", 1000, save_acumulada_plus_normal_ply);
-  ros::Subscriber subt    = nh.subscribe("/accumulated_termica", 1000, savetermica_ply);
-  ros::Subscriber subbool = nh.subscribe("/podemos_salvar_nuvens", 1000, salvando_nuvens);
+  ros::Subscriber sub     = nh.subscribe("/accumulated_point_cloud", 20, savevisual_ply);
+  ros::Subscriber subt    = nh.subscribe("/accumulated_termica", 20, savetermica_ply);
+  ros::Subscriber subbool = nh.subscribe("/podemos_salvar_nuvens", 20, salvando_nuvens);
+
+  mutexv = 0; mutext = 0;
 
   while(ros::ok()){
     ros::spinOnce();
